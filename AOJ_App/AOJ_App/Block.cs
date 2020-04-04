3892d38f7ee9df8d2902f58a897c1a7a9f9ab3f6
tomsoyaN <switchsoya2ss@gmail.com>
2020-04-05 01:51:40 +0900

    入力時各Blockオブジェクトの更新処理

---
+++ AOJ_App/AOJ_App/Block.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOJ_App
+{
+    class Block
+    {
+        public List<Block> blocks { get; set; }
+        public Block parent { get; set; }
+        public int startpos { get; set; }
+        public int endpos { get; set; }
+        public int startline { get; set; }
+        public int endline { get; set; }
+        public int depth { get; set; }
+    
+        public Block(Block par, int s, int e, int sl, int el, int d)
+        {
+            blocks = new List<Block>();
+            parent = par;
+            startpos = s;
+            startline = sl;
+            endline = el;
+            endpos = e;
+            depth = d;
+        }
+
+        public void Update(int c,int pos)
+        {
+            UpdatePos(c,pos);
+            UpdateLineNum(pos);
+        }
+        public void UpdatePos(int c,int pos)
+        {
+            if (parent != null)
+            {
+                if (pos <= startpos) startpos += c;
+                if (pos <= endpos) endpos += c;
+            }
+            foreach (var b in blocks)
+            {
+                b.UpdatePos(c, pos);
+            }
+            
+        }
+        public void UpdateLineNum(int pos)
+        {
+            if (parent != null)
+            {
+                if (pos <= startpos) startline++;
+                if (pos <= endpos) endline++;
+            }
+            foreach (var b in blocks)
+            {
+                b.UpdateLineNum(pos);
+            }
+
+        }
+        public void GetLineNum(string text)
+        {
+            int pos = 0, line = 1;
+            while(pos <= endpos)
+            {
+                if (pos == startpos) startline = line;
+                if (text[line] == '\n') line++;
+                pos++;
+            }
+            endline = line;
+        }
+    }
+}
--- AOJ_App/AOJ_App/codeeditor.cs
+++ AOJ_App/AOJ_App/codeeditor.cs
@@ -15,13 +15,20 @@ namespace AOJ_App
     {
         ICSharpCode.AvalonEdit.TextEditor editor;
         private CompletionWindow completewind;
-
+        private Block txts;
+        private Block curblock;
+        private const int nil = Int32.MinValue;
         public codeeditor(ICSharpCode.AvalonEdit.TextEditor e)
         {
             editor = e;
             editor.TextArea.TextEntered += TextArea_TextEntered;
             editor.TextArea.TextEntering += TextArea_TextEntering;
+            editor.TextArea.SelectionChanged += TextArea_SelectionChanged;
+            txts = new Block(null,nil,nil,nil,nil,0);
+            curblock = txts;
         }
+
+
 
         private void insertText(int pos, string tex)
         {
@@ -43,6 +50,8 @@ namespace AOJ_App
 
         private async void TextArea_TextEntered(object sender, TextCompositionEventArgs e)
         {
+            curblock = GetCurBlock(editor.SelectionStart-1);
+            txts.UpdatePos(e.Text.Length, editor.SelectionStart-1);
             if (e.Text == ".")
             {
                 completewind = new CompletionWindow(editor.TextArea);
@@ -59,9 +68,52 @@ namespace AOJ_App
                 insertText(editor.SelectionStart, ")");
             } else if (e.Text == "{")
             {
-                insertText(editor.SelectionStart, "\n}");
+                txts.UpdatePos(3, editor.SelectionStart-1);
+                txts.UpdateLineNum(editor.SelectionStart-1); txts.UpdateLineNum(editor.SelectionStart-1);
+                curblock.blocks.Add(new Block(curblock, editor.SelectionStart - 1, editor.SelectionStart+2, GetCurrentLineNum(), GetCurrentLineNum()+2, curblock.depth + 1));
+                insertText(editor.SelectionStart, "\n\n}");
+            } else if (e.Text == "\n")
+            {
+                txts.UpdateLineNum(editor.SelectionStart-1);
+            } else if (e.Text == "$")
+            {
+                System.Diagnostics.Debug.WriteLine(editor.SelectionStart);
             }
         }
+        private int GetCurrentLineNum()
+        {
+            int pos = 0, line = 1;
+            while (pos < editor.SelectionStart)
+            {
+                if (editor.Text.Length > pos && editor.Text[pos] == '\n') line++;
+                pos++;
+            }
+            return line;
+        }
+        private void TextArea_SelectionChanged(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(editor.SelectionStart);
+        }
 
+        private Block GetCurBlock(int pos)
+        {
+            bool flag = true ;
+            Block cur = new Block(null, nil, nil, nil, nil, 0);
+            cur = txts;
+            while (true)
+            {
+                flag = true;
+                foreach (Block o in cur.blocks)
+                {             
+                    if(o.startpos <= pos && o.endpos >= pos)
+                    {
+                            flag = false;
+                            cur = o;
+                            break;          
+                    }
+                }
+                if (flag) return cur;
+            }
+        }
     }
 }
