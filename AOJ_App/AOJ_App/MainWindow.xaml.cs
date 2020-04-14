80eb56fc32793f1067a63e933fbbcb005d63ea77
RYOTA IIJIMA <jimar884@gmail.com>
2020-04-14 12:10:09 +0900

    コンパイラの処理待ち

--- AOJ_App/AOJ_App/MainWindow.xaml.cs
+++ AOJ_App/AOJ_App/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace AOJ_App
             var Editor = new codeeditor(editor);
         }
 
-        private async void Compile_Click(object sender, RoutedEventArgs e)
+        private void Compile_Click(object sender, RoutedEventArgs e)
         {
 
             //エディタの文をcppファイルとして保存
@@ -44,16 +44,12 @@ namespace AOJ_App
             writer.Write(editor.Text);
             writer.Close();
 
-            //cppファイルが生成されるのを待つ(mms)
-            await Task.Delay(3000);
 
             //コンパイラ起動
             //二つ目の引数はコンパイルしたいcppファイルの保存場所およびファイル名
             //a.exeはAOJOffline\AOJ_App\AOJ_App\bin\Debugに保存される模様
-            System.Diagnostics.Process.Start("g++", @"C:\Users\Owner\source\repos\AOJOffline\AOJ_App\AOJ_App\bin\Debug\0000.cpp");
-
-            //exeファイルが生成されるのを待つ
-            await Task.Delay(3000);
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start("g++", @"C:\Users\Owner\source\repos\AOJOffline\AOJ_App\AOJ_App\bin\Debug\0000.cpp");
+            p.WaitForExit(10000);
 
             //生成された実行ファイルを実行
             //二つ目の引数は実行したいexeファイルの保存場所およびファイル名
