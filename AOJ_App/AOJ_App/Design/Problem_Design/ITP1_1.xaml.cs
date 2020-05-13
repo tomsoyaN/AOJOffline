c7d7cb97716eadd8c2f6856b82713b3cba5c856c
kurikinton105 <51431248+kurikinton105@users.noreply.github.com>
2020-05-13 19:53:47 +0900

    ITP1_1の選択ページの追加

---
+++ AOJ_App/AOJ_App/Design/Problem_Design/ITP1_1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AOJ_App.Design.Problem_Design
+{
+    /// <summary>
+    /// Interaction logic for ITP1_1.xaml
+    /// </summary>
+    public partial class ITP1_1 : UserControl
+    {
+        public ITP1_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
